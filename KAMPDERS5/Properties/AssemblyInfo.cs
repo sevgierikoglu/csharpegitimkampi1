19fb9be1a64f1d7764d15424a3a7ebfeea029987
sevgierikoglu <sevgierikoglu34@gmail.com>
2024-10-19 18:08:05 +0300

    KAMPDERS5

---
+++ KAMPDERS5/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KAMPDERS5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("KAMPDERS5")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("14345b76-2e4b-4d6c-8160-4fd4ae31c45c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KAMPDERSI5/Program.cs
@@ -0,0 +1,169 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAMPDERSI5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ALT ALTA YILDIZ OLUŞTURMA
+
+            //for (int i=1; i<=10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region YAN YANA YILDIZ OLUŞTURMA
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+            //----------------------------------------------------------------------------
+
+            #endregion
+
+            #region YAN YANA YILDIZ OLUŞTURMA HER SATIRDA 10 TANE OLSUN
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+
+            #endregion
+
+            #region DİK ÜÇGEN OLUŞTURMA
+
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //   for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region TERS DİK ÜÇGEN OLUŞTURMA
+
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region DİK VE TERS DİK ÜÇGEN BERABER
+
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int i = 10; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region BAKLAVA DİLİMİ
+
+            //int n = 5; //baklava diliminin kaç satırdam oluşaccağını belirledik
+
+            //// üst kısım
+            //for (int i = 1; i <= n; i++) 
+
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" "); // bu kısıma kadar olan kısım sağ taraftan boşluk bırakmak için kodlandı
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+            //// alt kıısm 
+            //for (int i=n-1; i>=1; i--) 
+            //{
+            //    for (int j=n-1; j>0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k=1; k<=2*i-1; k++)
+            //    {
+            //       Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region PİRAMİT DESENİ OLUŞTURMA
+
+            //int n = 5;
+            //for (int i=1; i<=n; i++)
+            //{
+            //    // boşluk için 
+            //    for (int j=n-i; j>0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    // yıldızlar için 
+            //    for (int k=1; k<=2*i-1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region TERS PİRAMİT OLUŞTURMA
+
+            //int n = 5;
+
+            //for(int i=n; i>=1; i--)
+            //{
+            //    //boşluklar
+            //    for (int j=n-i; j>0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    //yıldızlar
+            //    for (int k=1; k<=2*i-1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
